0630efb46b5844859c25a8a276976514cd9fe622
Alexis Kofman <alexis.kofman@gmail.com>
2016-08-30 20:30:16 +0200

    Add info feature

---
+++ info/index.js
@@ -0,0 +1,34 @@
+const glob = require('glob');
+const path = require('path');
+const tmp = require('tmp');
+const extractZip = require('extract-zip');
+const plist = require('simple-plist');;
+let plistFileName, info = {};
+
+const filterPlistFileName = (entry) => {
+  if(/\.plist*/.test(entry.fileName)){
+    plistFileName = entry.fileName;
+  }
+};
+
+const info = (callback) => {
+  glob('**/*.ipa', { cwd: __dirname }, (error, files) => {
+    if(files.length > 0) {
+      info.path = path.join(__dirname, files[0]);
+
+      tmp.dir(function _tempDirCreated(err, dirPath, cleanupCallback) {
+        if (err) callback(err);
+
+        extractZip(files[0], {dir: dirPath, onEntry: filterPlistFileName}, (err) => {
+          info.plist = plist.readFileSync(path.join(dirPath, plistFileName));
+          callback(err, info);
+        });
+      });
+    }
+    else {
+      callback(new Error('ipa not found'));
+    }
+  });
+};
+
+module.exports = info;
