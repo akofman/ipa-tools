91bd4ff348dd05c8bc3ff80e4b6f04872206f2d7
Alexis Kofman <alexis.kofman@gmail.com>
2016-09-06 22:08:16 +0200

    Add eslint config

--- info/cli.js
+++ info/cli.js
@@ -3,10 +3,10 @@
 // USAGE :
 // $ ipaInfo
 // $ ipaInfo --short
-const ipaInfo = require('./');
-const argv = require('minimist')(process.argv.slice(2));
-const chalk = require('chalk');
-const stripIndents = require('common-tags/lib/stripIndents');
+const ipaInfo = require('./')
+const argv = require('minimist')(process.argv.slice(2))
+const chalk = require('chalk')
+const stripIndents = require('common-tags/lib/stripIndents')
 
 if (argv._.length === 0) {
   ipaInfo.read(argv).then((info) => {
@@ -15,21 +15,21 @@ if (argv._.length === 0) {
       Build version: ${chalk.green(info.plist.CFBundleShortVersionString)}
       Bundle version: ${chalk.green(info.plist.CFBundleVersion)}
       Minimum OS version: ${chalk.green(info.plist.MinimumOSVersion)}
-      Path: ${chalk.green(info.ipaPath)}`);
+      Path: ${chalk.green(info.ipaPath)}`)
     } else {
-      console.log(info);
+      console.log(info)
     }
   }).catch((err) => {
-    console.log(chalk.red(err.message));
-    process.exit();
-  });
+    console.log(chalk.red(err.message))
+    process.exit()
+  })
 }
 
 if (argv._.length === 1 && argv._[0] === 'update') {
   ipaInfo.update(argv).then((result) => {
-    console.log(`updated IPA: ${chalk.green(result)}`);
+    console.log(`updated IPA: ${chalk.green(result)}`)
   }).catch((err) => {
-    console.log(chalk.red(err.message));
-    process.exit();
-  });
+    console.log(chalk.red(err.message))
+    process.exit()
+  })
 }
--- info/index.js
+++ info/index.js
@@ -1,84 +1,85 @@
-const glob = require('glob');
-const path = require('path');
-const tmp = require('tmp');
-const extractZip = require('extract-zip');
-const plist = require('simple-plist');
-const merge = require('deepmerge');
-const clone = require('clone');
-const fs = require('fs');
-const archiver = require('archiver');
+const glob = require('glob')
+const path = require('path')
+const tmp = require('tmp')
+const extractZip = require('extract-zip')
+const plist = require('simple-plist')
+const merge = require('deepmerge')
+const fs = require('fs')
+const archiver = require('archiver')
 
 const extractIpa = (options) => {
-  let result = {};
+  let result = {}
   const filterPlistFileName = (entry) => {
-    if(/\.plist*/.test(entry.fileName)){
-      result.plistFileName = entry.fileName;
+    if (/\.plist*/.test(entry.fileName)) {
+      result.plistFileName = entry.fileName
     }
-  };
+  }
   return new Promise((resolve, reject) => {
-    let ipaPattern = options && options.input || '**/*.ipa';
+    let ipaPattern = options && options.input || '**/*.ipa'
     glob(ipaPattern, { cwd: process.cwd() }, (error, files) => {
-      if (error) reject(error);
-      if(files.length > 0) {
-        result.path = path.join(process.cwd(), files[0]);
-        result.ipaName = files[0];
+      if (error) reject(error)
+      if (files.length > 0) {
+        result.path = path.join(process.cwd(), files[0])
+        result.ipaName = files[0]
         tmp.dir({ unsafeCleanup: true }, (err, dirPath, cleanupCallback) => {
-          if (err) reject(err);
-          result.extractDirPath = dirPath;
+          if (err) reject(err)
+          result.extractDirPath = dirPath
           extractZip(result.path, {dir: dirPath, onEntry: filterPlistFileName}, (err) => {
-            if (!result.plistFileName){
-              reject(new Error('No plist found'));
+            if (err) reject(err)
+            if (!result.plistFileName) {
+              reject(new Error('No plist found'))
             } else {
-              resolve(result);
+              resolve(result)
             }
-          });
-        });
+          })
+        })
+      } else {
+        reject(new Error('ipa not found'))
       }
-      else {
-        reject(new Error('ipa not found'));
-      }
-    });
-  });
-};
+    })
+  })
+}
 
 const read = (options) => {
   return extractIpa(options).then((result) => {
     return {
       ipaPath: result.path,
       plist: plist.readFileSync(path.join(result.extractDirPath, result.plistFileName))
-    };
-  });
-};
+    }
+  })
+}
 
 const update = (options) => {
   return extractIpa(options).then((result) => {
-    const plistPath = path.join(result.extractDirPath, result.plistFileName);
-    const plistObj = plist.readFileSync(plistPath);
-    const archive = archiver.create('zip', {});
-    let zipPath, updatedPlistObj, output, newProps = {};
+    const plistPath = path.join(result.extractDirPath, result.plistFileName)
+    const plistObj = plist.readFileSync(plistPath)
+    const archive = archiver.create('zip', {})
+    let updatedPlistObj
+    let output
+    let newProps = {}
 
     // Keep only plist props
-    for ( let prop in plistObj ) {
+    for (let prop in plistObj) {
       if (options[prop]) {
-        newProps[prop] = options[prop];
+        newProps[prop] = options[prop]
       }
     }
 
-    updatedPlistObj = merge(plistObj, newProps);
-    plist.writeBinaryFileSync(plistPath, updatedPlistObj);
+    updatedPlistObj = merge(plistObj, newProps)
+    plist.writeBinaryFileSync(plistPath, updatedPlistObj)
 
     return new Promise((resolve, reject) => {
       tmp.dir({ keep: true }, (err, zipDirPath) => {
-        if (err) reject(err);
-        zipPath = options && options.output && path.join( options.output, result.ipaName) || path.join(process.cwd(), result.ipaName);
-        output = fs.createWriteStream(zipPath);
-        archive.pipe(output);
-        archive.directory(path.join(result.extractDirPath, 'Payload'), 'Payload').finalize();
-        resolve(zipPath);
-      });
-    });
-  });
+        if (err) reject(err)
+        const zipPath = options && options.output && path.join(options.output, result.ipaName) || path.join(process.cwd(), result.ipaName)
+        output = fs.createWriteStream(zipPath)
+        archive.pipe(output)
+        archive.directory(path.join(result.extractDirPath, 'Payload'), 'Payload').finalize()
+        resolve(zipPath)
+      })
+    })
+  })
 }
 
-exports.read = read;
-exports.update = update;
+exports.read = read
+exports.update = update
