10bbe0f5b660e8ab3b99b2ef1a2f4c452af1cb8e
Alexis Kofman <alexis.kofman@gmail.com>
2016-09-11 12:40:04 +0200

    Add swifts upport

--- info/index.js
+++ info/index.js
@@ -4,9 +4,23 @@ const tmp = require('tmp');
 const extractZip = require('extract-zip');
 const plist = require('simple-plist');
 const merge = require('deepmerge');
-const fs = require('fs');
+const fs = require('fs-extra');
 const archiver = require('archiver');
 const Applesign = require('applesign');
+const mkdirp = require('mkdirp');
+const spawn = require('child_process').spawn;
+
+const getXcodeDevPath = () => {
+  return new Promise((resolve, reject) => {
+    const xcodeSelect = spawn('xcode-select', ['--print-path']);
+    xcodeSelect.stdout.on('data', function (data) {
+      resolve(data.toString().replace(/\r?\n|\r/g, ''));
+    });
+    xcodeSelect.stderr.on('data', function (data) {
+      reject(data.toString());
+    });
+  });
+};
 
 const extractIpa = (options) => {
   let result = {};
@@ -50,6 +64,41 @@ const read = (options) => {
   });
 };
 
+const addSwiftSupport = (signedZipPath, appName, ipaName, dylibPath, dylibs, options) => {
+  return new Promise((resolve, reject) => {
+    tmp.dir({ keep: true }, (err, extractDirPath, cleanupCallback) => {
+      if (err) reject(err);
+      const swiftSupport = path.join(extractDirPath, 'SwiftSupport');
+      const archive = archiver.create('zip', {});
+      const zipPath = options && options.output && path.join(options.output, ipaName) || path.join(process.cwd(), ipaName);
+      let output;
+
+      extractZip(signedZipPath, {dir: extractDirPath}, (err) => {
+        if (err) reject(err);
+
+        mkdirp(swiftSupport, (err) => {
+          if (err) reject(err);
+          dylibs.forEach((lib) => {
+            const dylib = path.join(dylibPath, lib);
+            try {
+              fs.copySync(dylib, path.join(swiftSupport, lib));
+              console.log(`copy ${dylib} to ${path.join(swiftSupport, lib)}`);
+            } catch (e) {
+              console.log(`WARN: ${path.join(dylibPath, lib)} not found`);
+            }
+          });
+          output = fs.createWriteStream(zipPath);
+          archive.pipe(output);
+          archive.directory(extractDirPath, '.').finalize();
+          archive.on('end', () => {
+            resolve(zipPath);
+          });
+        });
+      });
+    });
+  });
+};
+
 const update = (options) => {
   return extractIpa(options).then((result) => {
     const plistPath = path.join(result.extractDirPath, result.plistFileName);
@@ -70,34 +119,53 @@ const update = (options) => {
     plist.writeBinaryFileSync(plistPath, updatedPlistObj);
 
     return new Promise((resolve, reject) => {
-      tmp.dir({ keep: true }, (err, zipDirPath) => {
-        if (err) reject(err);
-        const zipPath = options && options.output && path.join(options.output, result.ipaName) || path.join(process.cwd(), result.ipaName);
-        output = fs.createWriteStream(zipPath);
-        archive.pipe(output);
-        archive.directory(path.join(result.extractDirPath, 'Payload'), 'Payload').finalize();
-        archive.on('end', () => {
-          const applesign = new Applesign({
-            identity: options.identity,
-            mobileprovision: options.mobileprovision,
-            outfile: zipPath,
-            withoutWatchapp: true
-          });
+      getXcodeDevPath().then((xcodeDevPath) => {
+        tmp.dir({ unsafeCleanup: true }, (err, zipDirPath) => {
+          if (err) reject(err);
+          const frameworks = path.join(result.extractDirPath, `Payload/${plistObj.CFBundleName}.app/Frameworks`);
+          const zipPath = path.join(zipDirPath, result.ipaName);
+          const dylibPath = path.join(xcodeDevPath, 'Toolchains/XcodeDefault.xctoolchain/usr/lib/swift/iphoneos');
 
-          const onEnd = (err, data) => {
-            if (err) {
-              reject(err);
-            } else {
-              resolve(zipPath);
-            }
-          };
+          fs.readdir(frameworks, (err, files) => {
+            if (err) reject(err);
 
-          applesign.signIPA(zipPath, onEnd)
-          .on('warning', (msg) => {
-            console.log('WARNING', msg);
-          })
-          .on('message', (msg) => {
-            console.log('msg', msg);
+            files.forEach((lib) => {
+              try {
+                fs.copySync(path.join(dylibPath, lib), path.join(frameworks, lib));
+              } catch (e) {
+                console.log(`WARN: ${path.join(dylibPath, lib)} not found`);
+              }
+            });
+
+            output = fs.createWriteStream(zipPath);
+            archive.pipe(output);
+            archive.directory(path.join(result.extractDirPath, 'Payload'), 'Payload').finalize();
+            archive.on('end', () => {
+              const applesign = new Applesign({
+                identity: options.identity,
+                mobileprovision: options.mobileprovision,
+                outfile: zipPath,
+                withoutWatchapp: true
+              });
+
+              const onEnd = (err, data) => {
+                if (err) {
+                  reject(err);
+                } else {
+                  addSwiftSupport(zipPath, plistObj.CFBundleName, result.ipaName, dylibPath, files, options).then((zipPath) => {
+                    resolve(zipPath);
+                  });
+                }
+              };
+
+              applesign.signIPA(zipPath, onEnd)
+              .on('warning', (msg) => {
+                console.log('WARNING', msg);
+              })
+              .on('message', (msg) => {
+                console.log('msg', msg);
+              });
+            });
           });
         });
       });
